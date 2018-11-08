c305b48bf3bc1fbe631893ab5f36864701777a19
nico <brun.nicolas21@gmail.com>
2018-11-08 21:05:57 +0100

    remove Replayer type

--- cmd/erdle/replay.go
+++ cmd/erdle/replay.go
@@ -8,8 +8,8 @@ import (
 	"time"
 
 	"github.com/busoc/erdle"
+	"github.com/juju/ratelimit"
 	"github.com/midbel/cli"
-	"golang.org/x/time/rate"
 )
 
 var replayCommand = &cli.Command{
@@ -25,11 +25,11 @@ func runReplay(cmd *cli.Command, args []string) error {
 	if err := cmd.Flag.Parse(args); err != nil {
 		return err
 	}
-	w, err := Replay(cmd.Flag.Arg(0), rate)
+	c, err := net.Dial(protoFromAddr(cmd.Flag.Arg(0)))
 	if err != nil {
 		return err
 	}
-	defer w.Close()
+	defer c.Close()
 
 	var rs []io.Reader
 	for i := 1; i < cmd.Flag.NArg(); i++ {
@@ -45,6 +45,8 @@ func runReplay(cmd *cli.Command, args []string) error {
 
 	when := time.Now()
 	vs := make([]byte, 1024)
+
+	w := ratelimit.Writer(c, ratelimit.NewBucketWithRate(rate.Float(), rate.Int()))
 	var n int64
 	for {
 		nn, err := io.CopyBuffer(w, r, vs)
@@ -55,41 +57,6 @@ func runReplay(cmd *cli.Command, args []string) error {
 			log.Printf("%s", err)
 		}
 	}
-	// n, err := io.CopyBuffer(w, r, vs)
 	log.Printf("%d KB sent in %s", n>>10, time.Since(when))
 	return nil
-}
-
-func Replay(addr string, z cli.Size) (net.Conn, error) {
-	c, err := net.Dial(protoFromAddr(addr))
-	if err != nil {
-		return nil, err
-	}
-	return Replayer(c, z), nil
-}
-
-func Replayer(c net.Conn, z cli.Size) net.Conn {
-	if z == 0 {
-		return c
-	}
-	return &replay{
-		Conn:    c,
-		limiter: rate.NewLimiter(rate.Limit(z.Float()), int(z.Int())/10),
-	}
-}
-
-type replay struct {
-	net.Conn
-	limiter *rate.Limiter
-}
-
-func (r *replay) Write(bs []byte) (int, error) {
-	v := r.limiter.ReserveN(time.Now(), len(bs))
-	if !v.OK() {
-		log.Printf("skipping %d bytes", len(bs))
-		return len(bs), nil
-	}
-	time.Sleep(v.Delay())
-
-	return r.Conn.Write(bs)
 }
