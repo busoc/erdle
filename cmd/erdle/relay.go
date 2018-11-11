751f049109afd6986d28b647d9db5e519b55f880
nico <brun.nicolas21@gmail.com>
2018-11-11 11:19:47 +0100

    move ring to ringbuffer package/repo

--- cmd/erdle/relay.go
+++ cmd/erdle/relay.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/busoc/erdle"
 	"github.com/midbel/cli"
+	"github.com/midbel/ringbuffer"
 )
 
 const (
@@ -92,7 +93,7 @@ func relayUDP(local, remote, proxy, mode string, size int) error {
 	}
 	defer r.Close()
 
-	return Relay(w, Ring(8<<20, r), proxy, mode, size)
+	return Relay(w, ringbuffer.Ring(8<<20, r), proxy, mode, size)
 }
 
 func Relay(w io.Writer, r io.Reader, proxy, mode string, size int) error {
@@ -203,89 +204,4 @@ func HRDL(w io.Writer) io.Writer {
 
 func (r *hrdlRelayer) Write(bs []byte) (int, error) {
 	return r.w.Write(bs)
-}
-
-type ring struct {
-	buffer []byte
-
-	rix   int
-	rchan chan int
-
-	wix   int
-	wchan chan int
-}
-
-func Ring(s int, rs io.Reader) io.ReadWriter {
-	r := ring{
-		buffer: make([]byte, s),
-		rchan:  make(chan int),
-		wchan:  make(chan int),
-	}
-	go r.syncboth(s / 2)
-	go r.fill(rs)
-	return &r
-}
-
-func (r *ring) Read(bs []byte) (int, error) {
-	r.rchan <- len(bs)
-	<-r.rchan
-	n := copy(bs, r.buffer[r.rix:])
-	if n < len(bs) {
-		r.rix = copy(bs[n:], r.buffer)
-	} else {
-		r.rix += n
-	}
-	return len(bs), nil
-}
-
-func (r *ring) Write(bs []byte) (int, error) {
-	n := copy(r.buffer[r.wix:], bs)
-	if n < len(bs) {
-		r.wix = copy(r.buffer, bs[n:])
-	} else {
-		r.wix += n
-	}
-	r.wchan <- len(bs)
-	return len(bs), nil
-}
-
-func (r *ring) fill(rs io.Reader) {
-	for {
-		_, err := io.Copy(r, rs)
-		if !erdle.IsErdleError(err) {
-			return
-		}
-	}
-}
-
-func (r *ring) syncboth(threshold int) {
-	var available int
-	for {
-		select {
-		case n, _ := <-r.wchan:
-			available += n
-			for available < threshold {
-				nn := <-r.wchan
-				available += nn
-			}
-		case n, _ := <-r.rchan:
-			if n < available {
-				available -= n
-				r.rchan <- n
-				break
-			}
-			for nn := range r.wchan {
-				available += nn
-				for available < threshold {
-					nn := <-r.wchan
-					available += nn
-				}
-				if n < available {
-					available -= n
-					r.rchan <- n
-					break
-				}
-			}
-		}
-	}
 }
